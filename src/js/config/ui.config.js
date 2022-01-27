c2946feecb7a53597d3a9dc444257c5d5b96efb6
qsegel <tarasoffmiti@gmail.com>
2022-01-27 18:22:39 +0300

    validate

---
+++ src/js/config/ui.config.js
@@ -0,0 +1,7 @@
+const UI = {
+  form: document.querySelector('.loginForm'),
+  inputEmail: document.querySelector('#email'),
+  inputPassword: document.querySelector('#password'),
+};
+
+export default UI;
